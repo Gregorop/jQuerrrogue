e71a78786fd2ba9069ab5243d173f1c7bebf5b5e
gregorop <gregorop@yandex.ru>
2025-06-30 23:03:10 +0300

    Не должно быть недостижимых зон

--- scripts/map.js
+++ scripts/map.js
@@ -12,9 +12,9 @@ function GameMap() {
                 this.grid[y][x] = "W";
             }
         }
-        this.room_generate()
-        this.paths_generate()
-        this.loot_generate()
+        this.paths_generate();
+        this.rooms_generate();
+        this.loot_generate();
     };
 
     this.randomX = function(){
@@ -26,15 +26,17 @@ function GameMap() {
     }
 
 
-    this.room_generate = function(){
+    this.rooms_generate = function(){
         var roomCount = 5 + Math.floor(Math.random() * 5);
         
         for (var i = 0; i < roomCount; i++) {
             var roomWidth = 3 + Math.floor(Math.random() * 5);
             var roomHeight = 3 + Math.floor(Math.random() * 5);
             
-            var posX = Math.floor(Math.random() * (this.widthN - roomWidth - 1)) + 1;
-            var posY = Math.floor(Math.random() * (this.heightN - roomHeight - 1)) + 1;
+            do {
+                var posX = Math.floor(Math.random() * (this.widthN - roomWidth - 1)) + 1;
+                var posY = Math.floor(Math.random() * (this.heightN - roomHeight - 1)) + 1;
+            } while (!this.cell_is_empty(posX, posY))
             
             for (var y = posY; y < posY + roomHeight; y++) {
                 for (var x = posX; x < posX + roomWidth; x++) {
