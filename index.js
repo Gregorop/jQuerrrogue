00bfaf91b701f3551e0238ede11b912e67848973
gregorop <gregorop@yandex.ru>
2025-06-30 22:58:45 +0300

    вражины

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function Game() {
     this.map = new GameMap();
     this.hero = new Hero(this.map);
+    this.enemies = [];
 
     this.init = function() {
         this.map.generate();
         this.hero.init();
         this.hero.setup_controls();
+        for (var i = 0; i < 10; i++) {
+            var enemy = new Enemy(this.map);
+            enemy.init();
+            this.enemies.push(enemy);
+        }
+
         this.gameLoop();
     };
 
     this.gameLoop = function() {
+        this.enemies.forEach(enemy => enemy.make_step());
         this.map.render();
         requestAnimationFrame(this.gameLoop.bind(this));
     };
---
+++ scripts/enemy.js
@@ -0,0 +1,27 @@
+function Enemy(map) {
+    this.step_prob = 0.05
+
+    this.init = function(){
+        this.map = map;
+        this.pos = map.get_random_empty_cell();
+        map.spawn_item(this.pos.x,this.pos.y,"E");
+    }
+
+    this.make_step = function(){
+        if (Math.random() < this.step_prob){
+            dx = Math.floor(Math.random() * 3) - 1;
+            dy = dx != 0 ? 0 : Math.floor(Math.random() * 3) - 1;
+            newX = this.pos.x+dx;
+            newY = this.pos.y+dy;
+            console.log(this.pos, newX, newY)
+            if (0 <= newX && newX < this.map.widthN && 0 <= newY && newY < this.map.heightN){
+                if (this.map.cell_is_empty(newX,newY)){
+                    this.map.grid[this.pos.y][this.pos.x] = "bg";
+                    this.map.grid[newY][newX] = "E";
+                    this.pos.x = newX;
+                    this.pos.y = newY;
+                }
+            }
+        }
+    }
+}
