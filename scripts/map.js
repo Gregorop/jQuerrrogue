6fad24757a6efc1bf0f28fb7747015d801038806
gregorop <gregorop@yandex.ru>
2025-06-30 21:33:44 +0300

    массовый спавн в свободных клетках

--- scripts/map.js
+++ scripts/map.js
@@ -14,7 +14,17 @@ function GameMap() {
         }
         this.room_generate()
         this.paths_generate()
+        this.loot_generate()
     };
+
+    this.randomX = function(){
+        return Math.floor(Math.random() * (this.widthN-2))+1;
+    }
+
+    this.randomY = function(){
+        return Math.floor(Math.random() * (this.heightN-2))+1;
+    }
+
 
     this.room_generate = function(){
         var roomCount = 5 + Math.floor(Math.random() * 5);
@@ -34,19 +44,48 @@ function GameMap() {
             }
         }
 
+    this.spawn_item = function(x,y,type){
+        if (this.grid[y][x] === "bg"){
+            this.grid[y][x] = type;
+            return true
+        }
+        else{
+            return false
+        }
+        
+    }
+
+    this.spawn_N_items = function(n,type){
+        items = 0
+        attempts = this.widthN * this.heightN
+        while (items < n && attempts > 0) {
+            attempts--;
+            var x = this.randomX();
+            var y = this.randomY();
+            if (this.spawn_item(x, y, type)){
+                items++
+            }
+        }
+    }
+
+    this.loot_generate = function(){
+        this.spawn_N_items(2, "SW");
+        this.spawn_N_items(10, "HP");
+    }
+
     this.paths_generate = function(){
         var horizontalN = 3 + Math.floor(Math.random() * 2);
         var verticalN = 3 + Math.floor(Math.random() * 2);
 
         for (var i = 0; i < verticalN; i++) {
-            var y = Math.floor(Math.random() * this.heightN-2)+1;
+            var y = this.randomY();
             for (var x = 0; x < this.widthN; x++) {
                 this.grid[y][x] = 'bg';
             }
         }
 
         for (var i = 0; i < horizontalN; i++) {
-            var x = Math.floor(Math.random() * this.widthN-2)+1;
+            var x = this.randomX();
             for (var y = 0; y < this.heightN; y++) {
                 this.grid[y][x] = 'bg';
             }
